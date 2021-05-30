d7a37e1bd33ad7649821d240a626520423710b4a
Zambon_matteo <matteo.zambon2004@gmail.com>
2021-05-30 14:24:15 +0200

    tutto finito tranne la parte di output che non capisco cosa non vada

--- campionato_f1.cpp
+++ campionato_f1.cpp
@@ -390,6 +390,11 @@ void bubbleSortp(Pilota* A[])
 			}
 		}
 	}
+	for(int h=0;h<SIZEpiloti;h++)
+		{
+		
+			cout <<"-"<< A[h]->nome <<" "<<A[h]->cognome <<" con "<<A[h]->punti <<" punti.\n";
+		}
 }
 
 void bubbleSorts(Scuderia* A[])
